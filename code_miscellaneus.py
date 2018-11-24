d4b54a77ce6322abc49d49cceb85cd37f36f3837
balrog <nona.h@seznam.cz>
2018-11-24 18:44:23 +0100

    Deleting abundant file while storing the code in another file.

--- code_miscellaneus.py
+++ code_miscellaneus.py
@@ -122,3 +122,11 @@ print(sum([i ** 2 for i in range(10)]))
 rozdil se da chapat tak, ze pokud je to rozepsane na hafo radku a postupne apendovane do seznamu, tak to rika spis
 JAK se to ma udelat, zatimco ve verzi comprehension to rika, CO to ma udelat.
 """
+
+
+# hvezdicky, slovniky, seznamy
+slovnik = {"jmeno": "Lucka", "povolani": "drticka odpadu", "zajmy": "kresleni"}
+print("{jmeno} pracuje jako {povolani} a ma rada {zajmy}.".format(**slovnik))
+
+seznam = ["Marek", "Matous", "Lukas", "Jan"]
+print("{0}, {1}, {2} a [3] si sedli a napsali knizku.".format(*seznam))
