9a695595413c902ae6cafb0d359c27c6ab907fe5
balrog-nona <nona.h@seznam.cz>
2019-01-12 18:33:53 +0100

    Turtle program improved

--- grafika_turtle/triCtverce.py
+++ grafika_turtle/triCtverce.py
@@ -1,19 +1,9 @@
-from turtle import forward, left, right, exitonclick, penup, pendown
+from turtle import forward, left, exitonclick
 
-for i in range(4):
-    forward(100)
-    left(90)
-
-left(135)
-forward(100 ** 0.5)
-left(90)
-forward(100)
-left(90)
-forward(100 ** 0.5)
-
-left(120)
-forward(50)
-right(90)
-forward(50)
+for i in range(3):
+    for y in range(4):
+        forward(100)
+        left(90)
+    left(20)
 
 exitonclick()
