0e0f280f00eeed6edb859f5a0c962bc91ffba481
balrog <nona.h@seznam.cz>
2019-04-12 18:45:45 +0200

    Functional version of counting kms for the future.

--- cycling_calendar/counting_future/main.py
+++ cycling_calendar/counting_future/main.py
@@ -4,12 +4,13 @@ from httplib2 import Http  # HTTP object for signed requests
 from oauth2client import file, client, tools  # for token storage
 import re
 import calendar_access
-import datetime
-import calendar
+import making_date
 
 """
 This script counts how many kms I did on exercise bike during a particular month + using "rotoped soucet" event in my 
 calendar creates an event on the last day of every month to my calendar with the total sum of kms in my whole history.
+
+The program will run every last day of the following months.
 
 Video about authorization:
 https://www.youtube.com/watch?v=h-gBeC9Y9cE
@@ -28,20 +29,13 @@ if not credz or credz.invalid:  # if the credentials are missing or invalid
 
 SERVICE = build("calendar", "v3", http=credz.authorize(Http()))  # creating an endpoint to that API; last par signes
 
-# creating dates to use in calling the calendar API
-first_day_current_month = datetime.date.today().replace(day=1).isoformat()
-last_day_current_month = datetime.date.today().isoformat()
-
-# Calling the Calendar API
-first_day_current_month_time = first_day_current_month + "T01:00:00Z"  # the format necessary according to the doc
-last_day_current_month = last_day_current_month + "T23:50:00Z"
+# 1. call to the Calendar API - gathering events from the current month to count
 calendar_ID = calendar_access.ID  # calendar Cviceni
-events_result = SERVICE.events().list(calendarId=calendar_ID, timeMax=last_day_current_month,
-                                      timeMin=first_day_current_month_time, maxResults=60, singleEvents=True,
+events_result = SERVICE.events().list(calendarId=calendar_ID, timeMax=making_date.time_max1,
+                                      timeMin=making_date.time_min1, maxResults=60, singleEvents=True,
                                       orderBy='startTime').execute()
-events = events_result.get('items', [])  # events from the current month
-for event in events:
-    print(event)
+events = events_result.get('items', [])  # type list
+
 
 def count_kms(iterable):
     """
@@ -52,56 +46,42 @@ def count_kms(iterable):
     """
     month_count = 0
     pattern = '\d+\.?\d+?\s?[kK]'
-    for item in iterable:
-        if "rotoped" in item["summary"] and "description" in item.keys():
-            comment_string = re.sub(",", ".", item["description"])
-            if re.search(pattern, comment_string):
-                kms = re.search(pattern, comment_string).group().lower()
-                kms = kms[:kms.find("k")]
-                kms = float(kms.strip())
-                # print(kms)
-                month_count += kms
-                # print(month_count)
+    if iterable:
+        for item in iterable:
+            if "rotoped" in item["summary"] and "description" in item.keys():
+                comment_string = re.sub(",", ".", item["description"])
+                if re.search(pattern, comment_string):
+                    kms = re.search(pattern, comment_string).group().lower()
+                    kms = kms[:kms.find("k")]
+                    kms = float(kms.strip())
+                    month_count += kms
     return month_count
 
-
-this_month = count_kms(events)
-print(this_month)
 
 """"
 Accessing the last event called "rotoped soucet" from previous month
 This event has a start date last day of previous month and an end date first day of current month
 """
-first_day_current_month_time2 = first_day_current_month + "T23:00:00Z"
-last_month_number = datetime.date.today().month - 1
-last_month = datetime.date.today().replace(day=1, month=last_month_number)
-last_day_previous_month = calendar.monthrange(last_month.year, last_month.month)[1]
-last_day_last_month = datetime.date.today().replace(day=last_day_previous_month, month=last_month_number).isoformat()
-last_day_last_month = last_day_last_month + "T01:00:00Z"
+# 2. call to the calendar API - accessing event "rotoped soucet" from previous month
+events_result2 = SERVICE.events().list(calendarId=calendar_ID, timeMax=making_date.time_max2,
+                                       timeMin=making_date.time_min2, maxResults=6, singleEvents=True).execute()
+events2 = events_result2.get('items', [])
 
-events_result = SERVICE.events().list(calendarId=calendar_ID, timeMax=first_day_current_month_time2,
-                                       timeMin=last_day_last_month, maxResults=6, singleEvents=True).execute()
-events = events_result.get('items', [])
-
-for event in events:
-    print(event)
+for event in events2:
     if "rotoped soucet" in event["summary"]:
         sum_up_to_last_month = event["description"]
         sum_up_to_last_month = sum_up_to_last_month[:sum_up_to_last_month.find("k")]
         sum_up_to_last_month = float(sum_up_to_last_month.strip())
 
+this_month = count_kms(events)
 total = this_month + sum_up_to_last_month
-print(total)
 
 # creating an event with the result - total kms
-next_month = datetime.date.today().month + 1  # type int
-next_month_first_day = datetime.date.today().replace(day=1, month=next_month).isoformat()
-
 EVENT = {
     "summary": "rotoped soucet",
     "description": "{} km".format(total),
-    "start": {"date": datetime.date.today().isoformat()},
-    "end": {"date": next_month_first_day}
+    "start": {"date": making_date.start_date},
+    "end": {"date": making_date.end_date}
 }
 
 e = SERVICE.events().insert(calendarId=calendar_ID, body=EVENT).execute()  # notification can be made as well
--- cycling_calendar/counting_future/making_date.py
+++ cycling_calendar/counting_future/making_date.py
@@ -1,13 +1,41 @@
 import datetime
+import calendar
 
-first_day = datetime.date.today().replace(day=1).isoformat()
-first_day = first_day + "T01:00:00Z"
+"""
+Creating dates to use in the FIRST call to the calendar API
+timeMax must be the last day of current month - which means the day of running the program.
+timeMin must be the first day of current month because I will gather the events from the whole current month.
+"""
+time_min1 = datetime.date.today().replace(day=1).isoformat()
+time_max1 = datetime.date.today().isoformat()
 
-last_day = datetime.date.today().isoformat()
-last_day = last_day + "T23:50:00Z"
+# the format necessary according to the documentation
+time_min1 = time_min1 + "T01:00:00Z"
+time_max1 = time_max1 + "T23:50:00Z"
 
+"""
+Creating dates to use in the SECOND call to the calendar API
+I'm looking for the last event called "rotoped soucet" which was created the previous month as a whole day event.
+timeMin must be the last day of last month.
+timeMax must be the first day of the current month because that's how google calendar works - the whole day events ends
+the next day.
+"""
+last_month_number = datetime.date.today().month - 1
+last_month = datetime.date.today().replace(day=1, month=last_month_number)
+# the day must be set to 1 because every month has day 1, but not every has day 31
+last_day = calendar.monthrange(last_month.year, last_month.month)[1]
+time_min2 = datetime.date.today().replace(day=last_day, month=last_month_number).isoformat()
+time_min2 = time_min2 + "T01:00:00Z"
 
-next_month = datetime.date.today().month + 1
-print(next_month, type(next_month))
-next_month_first_day = datetime.date.today().replace(month=next_month, day=1).isoformat()
-print(type(next_month_first_day))
+time_max2 = datetime.date.today().replace(day=1).isoformat() + "T23:00:00Z"
+
+"""
+Creating dates for the new event
+This event will be the whole day event which means:
+start must be today - last day of current month
+end must be tomorrow - the first day of next month
+"""
+
+start_date = datetime.date.today().isoformat()
+next_month_number = datetime.date.today().month + 1  # type int
+end_date = datetime.date.today().replace(day=1, month=next_month_number).isoformat()
