babb81539d6aa9726703ccf1328ef8674a0e2d4b
balrog-nona <nona.h@seznam.cz>
2019-07-19 12:34:05 +0200

    Part 1 of testing exercise
    Starting over with more elegant approach

--- testy_a_moduly/string_calculator.py
+++ testy_a_moduly/string_calculator.py
@@ -3,31 +3,15 @@ import pytest
 class String_calculator:
 
     def add(self, a):
-        if bool(a) == False:
+        if not a:
             return 0
         else:
-            if a[0] == "/":
-                delimiter = a[2]
-            else:
-                delimiter = ","
-            if "\n" in a:
-                a = a.replace("\n", delimiter)
-            if delimiter in a:
-                a = a.split(delimiter)
-                summary = 0
-                for i in a:
-                    try:
-                        if int(i) > 0:
-                            summary += int(i)
-                        else:
-                            raise ValueError("negatives not allowed")
-                            continue
-                    except ValueError:
-                        pass
-                return summary
-            else:
-                return int(a)
-
+            if ',' in a:
+                a = a.split(',')
+            sum = 0
+            for i in a:
+                sum += int(i)
+            return sum
 
 
 
