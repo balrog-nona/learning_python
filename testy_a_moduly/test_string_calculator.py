9e54602a1a641d93425ba4d6fba5e5f94225c539
balrog-nona <nona.h@seznam.cz>
2019-07-19 15:10:00 +0200

    Part 4 of testing exercise

--- testy_a_moduly/test_string_calculator.py
+++ testy_a_moduly/test_string_calculator.py
@@ -3,17 +3,20 @@ import pytest
 
 cal = string_calculator.String_calculator()
 
+
 def test_ex1v1(): #v1 stands for version 1
     inputs = ['', '2', '4,18', '2, 50']
     outputs = [0, 2, 22, 52]
     for input, output in zip(inputs, outputs):
         assert cal.add(input) == output
 
+
 def test_ex2v1():
     inputs = ['6, 7, 2', '4, 4, 4,9', '0,0, 1, 1, 1']
     outputs = [15, 21, 3]
     for input, output in zip(inputs, outputs):
         assert cal.add(input) == output
+
 
 def test_ex3v1():
     inputs = ['60\n3, 8\n1', '1\n0,6\n30, 4']
@@ -27,10 +30,18 @@ def test_ex3v1():
 def test_ex1(test_input, expected):
     assert cal.add(test_input) == expected
 
+
 @pytest.mark.parametrize('test_input, expected', [('6, 7, 2', 15), ('4,4,4, 9', 21), ('0,0, 1, 1, 1', 3)])
 def test_ex2(test_input, expected):
     assert cal.add(test_input) == expected
 
+
 @pytest.mark.parametrize('test_input, expected', [('6\n70\n2', 78), ('4,4\n4, 9', 21)])
 def test_ex3(test_input, expected):
+    assert cal.add(test_input) == expected
+
+
+list = [('//;\n1; 4; 50', 55), ('//~\n5~200', 205)]  #nevyhodou je, ze to je gloalni promenna
+@pytest.mark.parametrize('test_input, expected', list)
+def test_ex4(test_input, expected):
     assert cal.add(test_input) == expected
