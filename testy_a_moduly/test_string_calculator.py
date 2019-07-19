437c97f84769b4ad4df7ec5541d856d5aabe303d
balrog-nona <nona.h@seznam.cz>
2019-07-19 14:42:18 +0200

    Part 2 testing exercise

--- testy_a_moduly/test_string_calculator.py
+++ testy_a_moduly/test_string_calculator.py
@@ -3,9 +3,15 @@ import pytest
 
 cal = string_calculator.String_calculator()
 
-def test_ex1():
+def test_ex1v1(): #v1 jako verze 1
     inputs = ['', '2', '4,18', '2, 50']
     outputs = [0, 2, 22, 52]
+    for input, output in zip(inputs, outputs):
+        assert cal.add(input) == output
+
+def test_ex2v1():
+    inputs = ['6, 7, 2', '4, 4, 4,9', '0,0, 1, 1, 1']
+    outputs = [15, 21, 3]
     for input, output in zip(inputs, outputs):
         assert cal.add(input) == output
 
@@ -13,4 +19,8 @@ def test_ex1():
 
 @pytest.mark.parametrize('test_input, expected', [('', 0), ('2', 2), ('4,18', 22), ('2, 50', 52)])
 def test_ex1(test_input, expected):
+    assert cal.add(test_input) == expected
+
+@pytest.mark.parametrize('test_input, expected', [('6, 7, 2', 15), ('4,4,4, 9', 21), ('0,0, 1, 1, 1', 3)])
+def test_ex2(test_input, expected):
     assert cal.add(test_input) == expected
