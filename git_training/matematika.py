ec170bf778745b603974a8890a884aa241599525
balrog-nona <42897711+balrog-nona@users.noreply.github.com>
2019-08-08 13:49:28 +0200

    Pridane komentare

--- git_training/matematika.py
+++ git_training/matematika.py
@@ -3,24 +3,26 @@ import math
 class Matematika:
 
     def scitani(self, a, b):
+        # komentar
         return a + b
 
     def odcitani(self, a, b):
         return a - b
 
-    def nasobeni(self, a, b):
+    def nasobeni(self, a, b):  # tady se vysvetli princip metody
         return a * b
 
     def deleni(self, a, b):
         return a / b
 
-    def mocnina(self, a, b):
+    def mocnina(self, a, b):  # komentar
         return a ** b
 
     def odmocnina(self, a):
         return math.sqrt(a)
 
     def obvod_ctverce(self, a):
+        # komentar
         return 4 * a
 
     def obvod_obdelnika(self, a, b):
