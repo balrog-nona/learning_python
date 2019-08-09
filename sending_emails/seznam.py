e557b29acc56647de9bd5ee56185c3dde63e6ee4
balrog-nona <nona.h@seznam.cz>
2019-08-09 18:05:25 +0200

    Nastaveni spojeni se serverem

--- sending_emails/seznam.py
+++ sending_emails/seznam.py
@@ -3,20 +3,20 @@ import email_access
 import OpenSSL
 
 print('jdu na to')
-smtpObj = smtplib.SMTP('smtp.seznam.cz', 465)
-#smtpObj.set_debuglevel(1)
+smtpObj = smtplib.SMTP_SSL('smtp.seznam.cz', 465)  # encrypted SSL protocol
+smtpObj.set_debuglevel(True)
 print('mam server')
 # doma si zkusit pripojeni pres openssl - pozor na dlouhe pauzy
 
 #context = OpenSSL.create_default_context()
 #smtpObj.starttls(context=context)
-print(smtpObj.starttls())  # ensuring the encryption
+#print(smtpObj.starttls())  # ensuring the encryption
 print('mam sifrovani')
 print(smtpObj.ehlo())  # chci kod 250
 print('mam ehlo')
 print(smtpObj.login(email_access.seznam['email'], email_access.seznam['password']))  # chci kod 235
 
-smtpObj.sendmail(email_access.seznam['email'], email_access.gmail['email'], 'Subject: Hello\nDear Alice, '
+smtpObj.sendmail(email_access.seznam['email'], email_access.seznam['email'], 'Subject: Hello\nDear Alice, '
                                                                            'so long and thank you for all the fish. '
                                                                            'Sincerely, Fisheater')
 
