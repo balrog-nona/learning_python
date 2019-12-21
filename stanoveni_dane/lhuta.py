a5f963653ee5ee901794a7fbb917a6924e6731e6
balrog <nona.h@seznam.cz>
2019-12-21 21:45:39 +0100

    Zakladni kontrola odst.5

--- stanoveni_dane/lhuta.py
+++ stanoveni_dane/lhuta.py
@@ -6,11 +6,14 @@ class Lhuta:
     def __init__(self, zacatek):
         self.zacatek = self.na_americke_datum(zacatek)
         self.konec = self.zacatek.replace(year=self.zacatek.year + 3)
+        self.maximalni_delka = self.zacatek.replace(year=self.zacatek.year + 10)
 
     def odst2(self, datum):
         ukon = self.na_americke_datum(datum)
         if self.konec.replace(year=self.konec.year - 1) <= ukon <= self.konec:
             self.konec = self.konec.replace(year=self.konec.year + 1)
+        if not self.odst5():
+            self.konec = self.maximalni_delka
 
     def odst3(self, datum):
         ukon = self.na_americke_datum(datum)
@@ -18,12 +21,19 @@ class Lhuta:
             self.konec = self.konec.replace(year=self.konec.year + 3)
         else:
             raise Exception("Ukon ze dne {} zacal az po skonceni lhuty.".format(datum))
+        if not self.odst5():
+            self.konec = self.maximalni_delka
 
     def odst4(self, ode_dne, do_dne):
         zacatek_staveni = self.na_americke_datum(ode_dne)
         konec_staveni = self.na_americke_datum(do_dne)
         kolik_zbyvalo = (self.konec - zacatek_staveni) + datetime.timedelta(days=1)
         self.konec = konec_staveni + kolik_zbyvalo
+        if not self.odst5():
+            self.konec = self.maximalni_delka
+
+    def odst5(self):
+        return self.konec <= self.maximalni_delka
 
     def na_americke_datum(self, datum):
         den, mesic, rok = datum.split('.')
--- stanoveni_dane/test_lhuta.py
+++ stanoveni_dane/test_lhuta.py
@@ -8,6 +8,7 @@ def test_odst1():
     assert lhuta.zacatek == datetime.date(2020, 4, 1)
     assert lhuta.konec == datetime.date(2023, 4, 1)
     assert lhuta.vrat_konec() == "01.04.2023"
+    assert lhuta.maximalni_delka == datetime.date(2030, 4, 1)
 
 def test_odst2():
     lhuta.odst2("31.3.2022")
@@ -32,7 +33,12 @@ def test_odst4():
     lhuta.odst4(ode_dne='2.1.2024', do_dne='1.7.2024')
     assert lhuta.konec == datetime.date(2024, 8, 24)
 
-
-
-
-
+def test_odst5():
+    lhuta.odst3('3.5.2023')
+    lhuta.odst3('30.4.2026')
+    lhuta.odst3('1.2.2029')
+    assert lhuta.konec == datetime.date(2030, 4, 1)
+    lhuta.odst2('1.11.2029')
+    assert lhuta.konec == datetime.date(2030, 4, 1)
+    lhuta.odst4(ode_dne='1.12.2029', do_dne='25.3.2030')
+    assert lhuta.konec == datetime.date(2030, 4, 1)
