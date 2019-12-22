c9c42ad953edbea3a1c995ea70beb656303696e6
balrog <nona.h@seznam.cz>
2019-12-22 20:30:20 +0100

    Hotova zakladni verze vypoctu lhuty

--- stanoveni_dane/lhuta.py
+++ stanoveni_dane/lhuta.py
@@ -6,13 +6,14 @@ class Lhuta:
     Kontroluje zadani ukonu pred a po lhute.
     Neumi drzet v pameti historii ukonu, prepocitat lhutu po zruseni nektereho ukonu, a kvuli tomu neumi kontrolovat
     soubeh ruznych odstavcu.
-    Chybi kontrola vikendu.
     """
 
     def __init__(self, zacatek):
         self.zacatek = self.na_americke_datum(zacatek)
-        self.konec = self.zacatek.replace(year=self.zacatek.year + 3)  # normalne dle odst. 1
-        self.maximalni_delka = self.zacatek.replace(year=self.zacatek.year + 10)  # dle odst. 5
+        self.konec = self.zacatek.replace(year=self.zacatek.year + 3)  # normalne dle odst. 1; presne datum
+        self.konec = self.kontrola_vikendu(self.konec)  # konec lhuty po pravni strance
+        self.maximalni_delka = self.zacatek.replace(year=self.zacatek.year + 10)  # dle odst. 5; presne datum
+        self.maximalni_delka = self.kontrola_vikendu(self.maximalni_delka)  # konec lhuty po pravni strance
 
     def odst2(self, datum):
         ukon = self.na_americke_datum(datum)
@@ -20,6 +21,7 @@ class Lhuta:
             if self.konec.replace(year=self.konec.year - 1) <= ukon <= self.konec:
                 # kdyz nastal v poslednich 12 mesicich, prodluz lhutu o rok
                 self.konec = self.konec.replace(year=self.konec.year + 1)
+                self.konec = self.kontrola_vikendu(self.konec)
         if not self.odst5():
             self.konec = self.maximalni_delka
 
@@ -30,9 +32,9 @@ class Lhuta:
             # nelze jen tak zvysit cislo dne o 1, kazdy mesic ma jiny pocet dnu, hrozilo by 31.4.
             # vytvori nasledujici den prirozene podle kalendare
             druhy_den = ukon + datetime.timedelta(days=1)  # vraci datetime object
-            druhy_den = druhy_den.strftime('%d.%m.%Y')  # prevod datetime na string
-            druhy_den = self.na_americke_datum(druhy_den)  # prevod stringu na date object
+            druhy_den = self.prevod_data(druhy_den)  # prevod datetime na date object
             self.konec = druhy_den.replace(year=druhy_den.year + 3)
+            self.konec = self.kontrola_vikendu(self.konec)
         if not self.odst5():
             self.konec = self.maximalni_delka
 
@@ -46,6 +48,7 @@ class Lhuta:
             # 1 den je treba pricist rucne, aby lhuta nebezela i po oba hranicni dny
             kolik_zbyvalo = (self.konec - zacatek_staveni) + datetime.timedelta(days=1)  # timedelta object
             self.konec = konec_staveni + kolik_zbyvalo  # date object
+            self.konec = self.kontrola_vikendu(self.konec)
         if not self.odst5():
             self.konec = self.maximalni_delka
 
@@ -74,6 +77,19 @@ class Lhuta:
                             (self.na_ceske_datum(datum), self.na_ceske_datum(self.konec)))
         else:
             return True
+
+    def kontrola_vikendu(self, datum):  # datum je date object; lhuta nesmi skoncit o vikendu
+        if datum.isoweekday() == 7:  # nedele - lhuta konci v pondeli, tj. + 1 den
+            datum += datetime.timedelta(days=1)  # vraci datetime object
+            datum = self.prevod_data(datum)
+        elif datum.isoweekday() == 6:  # sobota - lhuta konci v pondeli, tj. + 2 dny
+            datum += datetime.timedelta(days=2)
+            datum = self.prevod_data(datum)
+        return datum  # vraci date object
+
+    def prevod_data(self, datum):  # datum je datetime object; prevede datetime object na date object
+        dat = datum.strftime('%d.%m.%Y')  # prevod datetime na string
+        return self.na_americke_datum(dat)  # vraci date object
 
     def vrat_konec(self):
         return self.na_ceske_datum(self.konec)
--- stanoveni_dane/test_lhuta.py
+++ stanoveni_dane/test_lhuta.py
@@ -6,56 +6,56 @@ import datetime
 def test_odst1():
     lhuta = Lhuta(zacatek='1.4.2020')
     assert lhuta.zacatek == datetime.date(2020, 4, 1)
-    assert lhuta.konec == datetime.date(2023, 4, 1)
-    assert lhuta.vrat_konec() == '01.04.2023'
+    assert lhuta.konec == datetime.date(2023, 4, 3)
+    assert lhuta.vrat_konec() == '03.04.2023'
     assert lhuta.maximalni_delka == datetime.date(2030, 4, 1)
 
 
 def test_odst2():
     lhuta = Lhuta(zacatek='1.4.2020')
-    lhuta.odst2('31.3.2022')
-    assert lhuta.konec == datetime.date(2023, 4, 1)
-    assert lhuta.vrat_konec() == '01.04.2023'
-    lhuta.odst2('1.4.2022')
-    assert lhuta.konec == datetime.date(2024, 4, 1)
-    assert lhuta.vrat_konec() == '01.04.2024'
-    lhuta.odst2('19.9.2023')
-    assert lhuta.vrat_konec() == '01.04.2025'
+    lhuta.odst2(datum='2.4.2022')
+    assert lhuta.konec == datetime.date(2023, 4, 3)
+    assert lhuta.vrat_konec() == '03.04.2023'
+    lhuta.odst2(datum='3.4.2022')
+    assert lhuta.konec == datetime.date(2024, 4, 3)
+    assert lhuta.vrat_konec() == '03.04.2024'
+    lhuta.odst2(datum='19.9.2023')
+    assert lhuta.vrat_konec() == '03.04.2025'
     with pytest.raises(Exception) as error:
-        lhuta.odst2('2.4.2025')
+        lhuta.odst2(datum='2.4.2025')
         assert 'Ukon ze dne 2.4.2025 nemuze nastat po konci behu lhuty dne {}.'.format\
             (lhuta.na_ceske_datum(lhuta.konec)) == str(error.value)
     with pytest.raises(Exception) as error:
-        lhuta.odst2('31.3.2020')
+        lhuta.odst2(datum='31.3.2020')
         assert 'Ukon ze dne 31.3.2020 nemuze nastat pred zapocetim behu lhuty dne {}.'.format\
                    (lhuta.na_ceske_datum(lhuta.zacatek)) == str(error.value)
 
 
 def test_odst3():
     lhuta = Lhuta(zacatek='1.4.2020')
-    lhuta.odst3('23.9.2021')
+    lhuta.odst3(datum='23.9.2021')
     assert lhuta.konec == datetime.date(2024, 9, 24)
     assert lhuta.vrat_konec() == '24.09.2024'
-    lhuta.odst3('24.9.2024')
-    assert lhuta.vrat_konec() == '25.09.2027'
+    lhuta.odst3(datum='24.9.2024')
+    assert lhuta.vrat_konec() == '27.09.2027'
     with pytest.raises(Exception) as error:
-        lhuta.odst3('26.9.2027')
+        lhuta.odst3(datum='26.9.2027')
         assert 'Ukon ze dne 26.9.2027 nemuze nastat po konci behu lhuty dne {}.'.format\
                    (lhuta.na_ceske_datum(lhuta.konec)) == str(error.value)
     with pytest.raises(Exception) as error:
-        lhuta.odst2('31.3.2020')
+        lhuta.odst2(datum='31.3.2020')
         assert 'Ukon ze dne 31.3.2020 nemuze nastat pred zapocetim behu lhuty dne {}.'.format \
                    (lhuta.na_ceske_datum(lhuta.zacatek)) == str(error.value)
-    lhuta.odst3('30.4.2023')
+    lhuta.odst3(datum='30.4.2023')
     assert lhuta.konec == datetime.date(2026, 5, 1)
 
 
 def test_odst4():
     lhuta = Lhuta(zacatek='1.4.2020')
     lhuta.odst4(ode_dne='1.5.2021', do_dne='25.3.2022')
-    assert lhuta.konec == datetime.date(2024, 2, 24)
+    assert lhuta.konec == datetime.date(2024, 2, 26)
     lhuta.odst4(ode_dne='2.1.2024', do_dne='1.7.2024')
-    assert lhuta.konec == datetime.date(2024, 8, 24)
+    assert lhuta.konec == datetime.date(2024, 8, 26)
     with pytest.raises(Exception) as error:
         lhuta.odst4(ode_dne='31.3.2020', do_dne=None)
         assert 'Ukon ze dne 31.3.2020 nemuze nastat pred zapocetim behu lhuty dne {}.'.format \
@@ -72,11 +72,11 @@ def test_odst4():
 
 def test_odst5():
     lhuta = Lhuta(zacatek='1.4.2020')
-    lhuta.odst3('3.5.2022')
-    lhuta.odst3('13.4.2025')
-    lhuta.odst3('1.2.2028')
+    lhuta.odst3(datum='3.5.2022')
+    lhuta.odst3(datum='13.4.2025')
+    lhuta.odst3(datum='1.2.2028')
     assert lhuta.konec == datetime.date(2030, 4, 1)
-    lhuta.odst2('1.11.2029')
+    lhuta.odst2(datum='1.11.2029')
     assert lhuta.konec == datetime.date(2030, 4, 1)
     lhuta.odst4(ode_dne='1.12.2029', do_dne='25.3.2030')
     assert lhuta.konec == datetime.date(2030, 4, 1)
@@ -103,3 +103,23 @@ def test_komplexni1():
     assert lhuta.vrat_konec() == '02.09.2027'
     lhuta.odst2(datum='1.2.2026')
     assert lhuta.vrat_konec() == '02.09.2027'
+
+
+def test_komplexni2():
+    lhuta = Lhuta('1.4.2019')
+    lhuta.odst4(ode_dne='6.12.2021', do_dne='11.7.2022')
+    assert lhuta.konec == datetime.date(2022, 11, 7)
+    lhuta.odst2(datum='10.7.2021')
+    assert lhuta.konec == datetime.date(2022, 11, 7)
+    lhuta.odst2(datum='22.3.2022')
+    assert lhuta.konec == datetime.date(2023, 11, 7)
+    lhuta.odst3(datum='17.11.2022')
+    assert lhuta.konec == datetime.date(2025, 11, 18)
+    lhuta.odst2(datum='12.12.2024')
+    assert lhuta.konec == datetime.date(2026, 11, 18)
+    with pytest.raises(Exception) as error:
+        lhuta.odst3(datum='12.12.2026')
+        assert 'Ukon ze dne 12.12.2026 nemuze nastat po konci behu lhuty dne {}.'.format \
+                   (lhuta.na_ceske_datum(lhuta.konec)) == str(error.value)
+    lhuta.odst3(datum='1.11.2026')
+    assert lhuta.konec == datetime.date(2029, 4, 2)
