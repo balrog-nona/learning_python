11c8ba4666afd4c38b5dad51ab481a5f2dcb72cf
balrog <nona.h@seznam.cz>
2020-07-26 12:19:33 +0200

    Vylepseni finalni zkousky

--- shell/sorter.sh
+++ shell/sorter.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+# final exam: 2000 files needs to be sorted to their respective directories based on their file type
+# report.csv contains information about original directory, new directory and file size
+
+
+echo "ORIGINAL_DIR,NEW_DIR,SIZE_BYTES" > report.csv
+
 for file in recovered/*
 do
-	if [[ $(file $file) == *"ASCII"* ]]
+	if [[ $(file "$file") == *"ASCII"* ]]  # varianta: if [[ $(file "$file" | grep ASCII) ]]
 	then
-		echo "/opt/$file,/opt/txt/$(basename $file),$(du -b $file | tr '\t' ' ' | cut -d' ' -f1)" >> report.csv
-		mv $file txt
-	elif [[ $(file $file) == *"image data"* ]]
+		DIR=txt
+	elif [[ $(file "$file") == *"image data"* ]]  # varianta: if [[ $(file "$file" | grep "image data") ]]
 	then
-		echo "/opt/$file,/opt/img/$(basename $file),$(du -b $file | tr '\t' ' ' | cut -d' ' -f1)" >> report.csv
-		mv $file img
-	elif [[ $(file $file) == *"executable"* ]]
+		DIR=img
+	elif [[ $(file "$file") == *"executable"* ]]  # varianta: if [[ $(file "$file" | grep "executable") ]]
 	then
-		echo "/opt/$file,/opt/bin/$(basename $file),$(du -b $file | tr '\t' ' ' | cut -d' ' -f1)" >> report.csv
-		mv $file bin	
+		DIR=bin	
 	fi
+	echo "/opt/$file,/opt/$DIR/$(basename $file),$(du -b $file | tr '\t' ' ' | cut -d' ' -f1)" >> report.csv
+	mv "$file" $DIR
 done
 
