031d08980e2cb80ab1d3c56fc48628dd4a77391e
balrog-nona <balrog@localhost.localdomain>
2020-09-12 18:08:26 +0200

    Zkontrolovano Pylintem

--- stanoveni_dane/lhuta_komplet/main.py
+++ stanoveni_dane/lhuta_komplet/main.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
+"""
+Funkce na kompletni vypocet lhuty pro stanoveni dane. Jednotlive ukonu
+jsou objety sve tridy a jsou do fce srovnane podle atributu ukon.
 
-from lhuta_komplet import Lhuta148, Odst1, Odst2, Odst3, Odst4
+Upraveny pripady soubehu odstavce 2, 3 a 4 ve staveni.
+Nakonec se na kazdem ukonu zavola metoda konec_lhuty.
+Vysledek z posledniho ukonu je konec lhuty pro stanoveni dane.
+"""
+
+
+from lhuta_komplet import Odst1
 from funkce import uprava_ukonu_pri_soubehu, uprava_odstavcu4, vytrid_odstavce4, vytrid_ukony
 
 
@@ -15,9 +24,9 @@ def spocitej_lhutu(seznam_ukonu):
 
     if not isinstance(seznam_ukonu[0], Odst1):
         raise Exception('Prvnim ukonem musi byt zahajeni behu lhuty dle § 148 odst.1.')
-    else:
-        seznam_ukonu[0].konec_lhuty()
-        seznam_ukonu[0].maximalni_delka()
+
+    seznam_ukonu[0].konec_lhuty()
+    seznam_ukonu[0].maximalni_delka()
     # zadani pocatecnich hodnot, od kterych se vyviji nasledny vypocet
     subjektivni_lhuta = seznam_ukonu[0].konec
     objektivni_lhuta = seznam_ukonu[0].maximalni
@@ -34,17 +43,13 @@ def spocitej_lhutu(seznam_ukonu):
         ukony_soubehu, seznam_ukonu = vytrid_ukony(seznam_odstavce4=odstavce4, seznam_ukonu=seznam_ukonu)
         if ukony_soubehu:
             seznam_ukonu = uprava_ukonu_pri_soubehu(odstavce4, ukony_soubehu, seznam_ukonu, subjektivni_lhuta,
-                                                objektivni_lhuta)
+                                                    objektivni_lhuta)
         else:  # kdyz neni zadny soubeh, tak se odstavce4 vrati do seznamu ukonu
             [seznam_ukonu.append(odstavec) for odstavec in odstavce4 if odstavce4]
 
     for ukon in seznam_ukonu[1:]:
         ukon.konec_lhuty(datum_zacatku=seznam_ukonu[0].ukon, datum_konce=subjektivni_lhuta,
-                      maximalni_delka=objektivni_lhuta)
+                         maximalni_delka=objektivni_lhuta)
         subjektivni_lhuta = ukon.konec
 
     return subjektivni_lhuta
-
-
-
-
